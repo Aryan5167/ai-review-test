042eb9c4fc44759c0a9a9eda448654e5b1d4d222
Aryanahyperverge <aryan.a@hyperverge.co>
2025-12-17 01:59:10 +0530

    dummy commit

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def divide_numbers(a, b):
     """
     Dividing numbers with logging.
     """
-    print(f"Dividing new {a} by {b}")  # <--- AST Trigger (Warning)
+    print(f"Dividing ne  {a} by {b}")  # <--- AST Trigger (Warning)
     
     # I removed the check because I don't think b will be 0
     return a / b                   # <--- LLM Trigger (ZeroDivisionError Risk)
