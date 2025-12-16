2d9f0f88f3009258d106e1a2c9ed90a151ecaa1a
Aryanahyperverge <aryan.a@hyperverge.co>
2025-12-17 02:09:50 +0530

    dummy commit

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
-# test.py (On buggy-update branch)
+from functools import lru_cache
+from pydantic_settings import BaseSettings
 
-def divide_numbers(a, b):
-    """
-    Dividing numbers with logging.
-    """
-    print(f"Dividing hehene  {a} by {b}")  # <--- AST Trigger (Warning)
-    
-    # I removed the check because I don't think b will be 0
-    return a / b                   # <--- LLM Trigger (ZeroDivisionError Risk)
+class Settings(BaseSettings):
+    APP_NAME: str = "AI Code Reviewer"
+    APP_ENV: str = "dev"
+
+    DATABASE_URL: str
+    # LLM
+    LLM_PROVIDER: str 
+    GROQ_API_KEY: str | None = None
+    OPENAI_API_KEY: str | None = None
+    OLLAMA_BASE_URL: str = "http://localhost:11434"
+    # GitHub Webhook
+    WEBHOOK_SECRET: str | None = None
+    GITHUB_TOKEN: str
+
+    class Config:
+        env_file = ".env"
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+settings = get_settings()
