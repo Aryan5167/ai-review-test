37ab01055653a34b6d10469ba0c46e5478fe320b
Aryan5167 <111250537+Aryan5167@users.noreply.github.com>
2025-12-18 01:18:31 +0530

    check

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ def divide_numbers(a, b):
     Dividing numbers with logging.
     """
     print(f"Dividing hehene  {a} by {b}")  # <--- AST Trigger (Warning)
-    
+    a=0
     # I removed the check because I don't think b will be 0
-    return 0 / 0                   # <--- LLM Trigger (ZeroDivisionError Risk)
+    return a / b                   # <--- LLM Trigger (ZeroDivisionError Risk)
